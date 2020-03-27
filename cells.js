2113b61650d2fc035ac557923f3eee5df884f8fc
Grace Mullins <gmullins0728@gmail.com>
2020-03-26 21:18:24 -0400

    add ts file

---
+++ cells.js
@@ -0,0 +1,27 @@
+//Randomly assign 'it' to one of the 'cell'
+var generateRandom = Math.floor(Math.random() * 15);
+//Implement queryselector
+var cells = document.querySelectorAll(".cell");
+//Assigned 'it' to 'cell'
+cells[generateRandom].id = 'it';
+//Created a list of 'cell' that have been clicked
+var clickedCells = [];
+//Created a function to change color
+var changeColor = function (event) {
+    if (event.target.classList.contains('cell')) {
+        event.target.style.backgroundColor = 'green';
+        clickedCells.push(event.target);
+    }
+    if (event.target.id === 'it') {
+        for (var _i = 0, clickedCells_1 = clickedCells; _i < clickedCells_1.length; _i++) {
+            var cells_1 = clickedCells_1[_i];
+            cells_1.style.backgroundColor = 'yellow';
+        }
+        event.target.style.backgroundColor = 'red';
+        event.target.parentElement.innerHTML = event.target.parentElement.innerHTML;
+    }
+};
+// Add function to the 'cell'
+for (var i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', function (event) { return changeColor(event); });
+}
---
+++ cells.ts
@@ -0,0 +1,44 @@
+
+//Randomly assign 'it' to one of the 'cell'
+const generateRandom: number = Math.floor(Math.random() * 15 );
+
+//Implement queryselector
+const cells: NodeListOf<Element> = document.querySelectorAll(".cell");
+
+
+//Assigned 'it' to 'cell'
+cells[generateRandom].id = 'it';
+
+//Created a list of 'cell' that have been clicked
+const clickedCells: any[] = [];
+
+interface ClickedCells {
+    backgroundColor: string;
+}
+
+//Created a function to change color
+const changeColor = (event) => {
+    if (event.target.classList.contains('cell')){
+        event.target.style.backgroundColor = 'green';
+        clickedCells.push(event.target);
+    }
+
+    if ( event.target.id === 'it') {
+        for (let cells of clickedCells) {
+            cells.style.backgroundColor = 'yellow';
+        }
+    
+    event.target.style.backgroundColor = 'red';
+    event.target.parentElement.innerHTML = event.target.parentElement.innerHTML;
+    }
+    
+}
+// Add function to the 'cell'
+for (let i =0; i < cells.length; i++) {
+    cells[i].addEventListener('click', (event) => changeColor(event)); 
+}
+
+
+
+
+
